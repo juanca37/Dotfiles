153889640cdc3537bf83f0039547eb78e5f73abf
juanca37 <73541975+juanca37@users.noreply.github.com>
2024-06-07 15:26:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-stow -v nvim zsh tmux
+
+mkdir ~/.config/nvim
+mkdir ~/.config/tmux
+
+sudo apt-get install ripgrep
+git clone https://github.com/juanca37/.nvim
+git clone https://github.com/juanca37/tmux
+git clone https://github.com/juanca37/zsh
+
+mv .nvim/.config/nvim/** ~/.config/nvim
+mv tmux/.config/tmux/** ~/.config/tmux
+mv zsh/.zshrc ~/.zshrc
+
