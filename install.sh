63ce5e3229c4dcc000286f3a1f8bf889b2986d50
juanca37 <73541975+juanca37@users.noreply.github.com>
2024-06-17 17:23:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir ~/.config/nvim
 mkdir ~/.config/tmux
 
-sudo apt-get update && sudo apt-get install neovim ripgrep zsh
+sudo apt-get update && sudo apt-get install neovim ripgrep zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 
 git clone https://github.com/juanca37/.nvim
