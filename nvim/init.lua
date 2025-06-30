30ac056e866a28ef5d7b3c872c46a4ea13ecd424
juanca37 <juancarrascoalonso@hotmail.com>
2025-06-30 16:23:55 +0200

    Adding treesitter to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,4 @@ require("config.lazy")
 require("keymaps")
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+				"nvim-treesitter/nvim-treesitter",
+				build = ":TSUpdate",
+				config = function()
+								local configs = require("nvim-treesitter.configs")
+								configs.setup({
+												highlight = {
+																enable = true
+												},
+												indent = {
+																enable = true
+												},
+												ensure_installed = { "lua", "python" }
+								})
+				end
+}
